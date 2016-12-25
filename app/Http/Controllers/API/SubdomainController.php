dddcb2fba1ce3c24b27ae09a51d0217fcbb74494
Chris Piggott <cpiggott@ksu.edu>
2016-12-25 02:58:31 -0600

    Added status creation to subdomain

--- app/Http/Controllers/API/SubdomainController.php
+++ app/Http/Controllers/API/SubdomainController.php
@@ -8,15 +8,26 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Models\Subdomain;
+use App\Models\Status;
 
 class SubdomainController extends Controller
 {
     public function postSubdomain(Request $request){
+
         $subdomain = Subdomain::create([
             'name' => $request->input('name'),
             'yes_protected' => false,
             'no_protected' => false
         ]);
+
+        //Create a status as well.
+        $status = Status::create([
+            'is_yes' => false,
+            'subdomain_id' => $subdomain->id
+        ]);
+
+        $subdomain->status = $status;
+
 
         return $subdomain;
     }
